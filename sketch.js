d4fa3dc26dad6521b059fa1515ff0c79891c00b2
arathorn76 <90253767+arathorn76@users.noreply.github.com>
2021-09-21 10:51:01 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,20 @@
 var board;
 
-let view;
-var sliderX;
-var sliderY;
-var sliderZ;
+let debug = true;
 
-let slider1;
-let slider2;
-let slider3;
+let view;
+let div2d3d;
+let div1, div2, div3;
+
+let slider3DView;
+var sliderX, sliderY, sliderZ;
+
+let players = [];
+
+let control;
+let buttonRestart;
+
+let player1, player2;
 
 var myFont;
 function preload() {
@@ -16,67 +23,114 @@ function preload() {
 
 function setup() {
   createCanvas(600, 600, WEBGL);
-  board = new Board(4, 40);
 
   textFont(myFont);
   textSize(30);
 
-  view = createDiv("view controls<br />");
-  slider1 = createDiv("X rotation");
-  slider1.parent(view);
-  sliderX = createSlider(PI * -1, PI, -0.75, 0.1);
-  sliderX.parent(slider1);
-  slider2 = createDiv("Y rotation");
-  slider2.parent(view);
-  sliderY = createSlider(PI * -1, PI, -0.35, 0.1);
-  sliderY.parent(slider2);
-  slider3 = createDiv("Z rotation");
-  slider3.parent(view);
-  sliderZ = createSlider(PI * -1, PI, -0.05, 0.1);
-  sliderZ.parent(slider3);
+  createGameControls();
 
-
-  //initialize display
-  //background(80);
-  //board.show2d();
+  startGame();
+  
+  frameRate(5);
 }
 
 function draw() {
   background(80);
-  board.show2d();
-
-  if (!board.movePossible) {
-    console.log("TIE!");
-    noLoop();
-    return;
+  if (slider3DView.value() === 0) {
+    board.show2d();
   } else {
-    if (board.activePlayer === 1) {
-      // player 1 = randomAI
-      while (!board.play(floor(random(4)), floor(random(4)), floor(random(4))));
-    }
+    board.show3d();
   }
 
   fill(255, 0, 0);
   if (board.winner) {
     text("WINNER: " + board.activePlayer, 0, -height / 3);
     view.remove();
+    noLoop();
   } else {
     text("next Player: " + board.activePlayer, 0, -height / 3);
   }
+  
+  if (!board.movePossible) {
+    console.log("TIE!");
+    noLoop();
+    return;
+  } else {
+    //if (board.activePlayer === 1) {
+      // player 1 = randomAI
+      players[board.activePlayer - 1].makeMove();
+    //}
+  }
+
 }
 
 function mousePressed() {
-  if (board.movePossible() && board.activePlayer === 2) {
+  // ignore mouse button events outside the canvas
+  if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height) {
+    return;
+  }
+
+  // process human moves - 2D version
+  if (
+    board.movePossible() &&
+    players[board.activePlayer - 1].type === `HUMAN` &&
+    slider3DView.value() === 0
+  ) {
     // player 2 = human
-    var x = mouseX - width / 2;
-    var y = mouseY - height / 2;
     let move = false;
-    while(!move) {
-      move = board.clicked2d(x, y);
-    } 
-    
-    board.play(move.x, move.y, move.z);
-    
+    let x = mouseX - width / 2;
+    let y = mouseY - height / 2;
+    move = board.clicked2d(x, y);
+    if (move) {
+      board.play(move.x, move.y, move.z);
+    }
   }
 }
 
+function createViewControls() {
+  if(view) view.remove();
+  view = createDiv("view controls<br />");
+  div2d3d = createDiv("Ansicht 2D/3D");
+  div2d3d.parent(view);
+  slider3DView = createSlider(0, 1, 0, 1);
+  slider3DView.parent(view);
+  div1 = createDiv("X rotation");
+  div1.parent(view);
+  sliderX = createSlider(PI * -1, PI, -0.75, 0.1);
+  sliderX.parent(div1);
+  div2 = createDiv("Y rotation");
+  div2.parent(view);
+  sliderY = createSlider(PI * -1, PI, -0.35, 0.1);
+  sliderY.parent(div2);
+  div3 = createDiv("Z rotation");
+  div3.parent(view);
+  sliderZ = createSlider(PI * -1, PI, -0.05, 0.1);
+  sliderZ.parent(div3);
+}
+
+function createGameControls() {
+  control = createDiv("game controls");
+  buttonRestart = createButton(`restart game`);
+  buttonRestart.parent(control);
+  buttonRestart.mouseClicked(startGame);
+  player1 = createSelect();
+  player1.option(`HUMAN`);
+  player1.position(5,height - 30);
+  player1.option(`RANDOM`);
+  player1.option(`AI1`);
+  player1.selected(`RANDOM`);
+  player2 = createSelect();
+  player2.position(150,height - 30);
+  player2.option(`HUMAN`);
+  player2.option(`RANDOM`);
+  player2.option(`AI1`);
+  player2.selected(`RANDOM`);
+}
+
+function startGame() {
+  createViewControls();
+  board = new Board(4, 40);
+  players[0] = new playerAI(player1.value(), 1);
+  players[1] = new playerAI(player2.value(), 2);
+  loop();
+}
