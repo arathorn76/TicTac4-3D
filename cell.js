93e048d2825527fdda037f3ee8a248b7b6b0c8d4
arathorn76 <90253767+arathorn76@users.noreply.github.com>
2021-09-21 10:49:44 +0200

    Update cell.js

--- cell.js
+++ cell.js
@@ -1,13 +1,10 @@
 class Cell {
-  constructor(x, y, z, size) {
-    this.xind = x / size + 2;
-    this.yind = y / size + 2;
-    this.zind = z / size + 2;
-
+  constructor(x, y, z, size, gridsize) {
     this.x = x;
     this.y = y;
     this.z = z;
     this.size = size;
+    this.gridsize = gridsize;
     this.state = 0;
 
     this.scale2d = 0.9;
@@ -23,11 +20,19 @@ class Cell {
       return false;
     }
   }
+  undo() {
+    if (this.state === 0) {
+      return false;
+    } else {
+      this.state = 0;
+      return true;
+    }
+  }
 
   // functions specific to 2D-mode
   show2d() {
-    let x = this.xpos2d();//(this.xind + 4 * this.zind - 8) * this.size2d;
-    let y = (this.yind + 4 * this.zind - 8) * this.size2d;
+    let x = this.xpos2d();
+    let y = (this.y + 4 * this.z - 8) * this.size2d;
 
     if (this.state === 1) {
       noFill();
@@ -56,44 +61,54 @@ class Cell {
 
     rect(x, y, this.size2d * this.scale2d);
 
-    // mark cell 0,0,0
-    if (this.xind === 0 && this.yind === 0 && this.zind === 0) {
-      fill(0, 0, 255);
-      ellipseMode(CENTER);
-      ellipse(
-        x + (this.size2d / 2) * this.scale2d,
-        y + (this.size2d / 2) * this.scale2d,
-        (this.size2d / 2) * this.scale2d
-      );
+    if (debug) {
+      // mark cell 0,0,0
+      if (this.x === 0 && this.y === 0 && this.z === 0) {
+        fill(0, 0, 255);
+        ellipseMode(CENTER);
+        ellipse(
+          x + (this.size2d / 2) * this.scale2d,
+          y + (this.size2d / 2) * this.scale2d,
+          (this.size2d / 2) * this.scale2d
+        );
+      }
     }
   }
 
   clicked(mx, my) {
     //let size2d = min(width / 16, height / 16);
     let x = this.xpos2d();
-    let y = (this.yind + 4 * this.zind - 8) * this.size2d;
+    let y = (this.y + 4 * this.z - 8) * this.size2d;
 
-    if (mx >= x && mx <= (x + (this.size2d * this.scale2d))) {
-      if (my >= y && my <= (y + (this.size2d  * this.scale2d))) {
+    if (mx >= x && mx <= x + this.size2d * this.scale2d) {
+      if (my >= y && my <= y + this.size2d * this.scale2d) {
         return true;
-        box(x,y,(x + this.size2d) * this.scale2d );
+        box(x, y, (x + this.size2d) * this.scale2d);
       }
     }
     return false;
   }
-  
-  xpos2d(){
-    let x = (this.xind + 4 * this.zind - 8) * this.size2d;
+
+  xpos2d() {
+    let x = (this.x + this.gridsize * this.z - this.gridsize * 2) * this.size2d;
     return x;
   }
+
+  ypos2d() {
+    let y = (this.y + this.gridsize * this.z - this.gridsize * 2) * this.size2d;
+    return y;
+  }
+
   // functions specific to 3D-mode
   show3d() {
-    //rect(this.x , this.y , this.size);
+    let x = this.pos3d(this.x);
+    let y = this.pos3d(this.y);
+    let z = this.pos3d(this.z);
     push();
     rotateX(sliderX.value());
     rotateY(sliderY.value());
     rotateZ(sliderZ.value());
-    translate(this.x, this.y, this.z);
+    translate(x, y, z);
     if (this.state === 1) {
       fill(255, 100, 100, 100);
       sphere(this.size / 2);
@@ -104,7 +119,12 @@ class Cell {
       noFill();
       box(this.size);
     }
-
     pop();
   }
+
+  pos3d(dimension){
+    let pos = (dimension - this.gridsize * .5) * this.size;
+    return pos;
+  }
+
 }
