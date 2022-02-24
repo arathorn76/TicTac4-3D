4c651bd1253d3d94b4a102d3102e6a81b788e24e
arathorn76 <90253767+arathorn76@users.noreply.github.com>
2022-02-25 00:44:30 +0100

    Implemented play-by-play evaluation

--- evaluation.js
+++ evaluation.js
@@ -3,9 +3,14 @@ class Evaluation {
     this.lines = [];
     this.values = [];
 
-    // TODO 3* 16 Linien parallel zu den Achsen
-    // TODO 6* 4 FlächenDiagonalen
-    // TODO 4 Raumdiagonalen
+    this.evals = {
+      row1: 1,
+      row2: 3,
+      row3: 10,
+      row4: 10000,
+      multirow3: 10,
+      noGood: 0,
+    };
   }
 
   addLines(cell) {
@@ -28,7 +33,7 @@ class Evaluation {
       }
       if (pushIt) {
         this.lines.push(newLine);
-        this.values.push(new Values());
+        this.values.push(new Values(this.values.length));
       }
     }
   }
@@ -38,33 +43,94 @@ class Evaluation {
     // find all lines containing cell
     for (let i = 0; i < this.lines.length; i++) {
       let update = false;
-      for (let point of (line = this.lines[i])) {
+      for (let point of this.lines[i]) {
         if (vec.equals(point)) {
           // evaluate each of those lines
+          // and set the values for each evaluated line
           this.evalLine(i);
+          break;
         }
       }
     }
-
-    // set the values for each evaluated line
   }
 
   evalLine(idx) {
-    let lineVal = new Values();
+    let lineVal = new Values(idx);
+    let counterP1 = 0;
+    let counterP2 = 0;
     // line evaluation
-    lineVal.player1 += 5;
-    lineVal.player2 += 5;
+    // lineVal.player1 += 5;
+    // lineVal.player2 += 5;
+    let evalLine = this.lines[idx];
+    for (let c of evalLine) {
+      switch (board.cells[ix(c.x, c.y, c.z)].state) {
+        case 1:
+          counterP1++;
+          break;
+        case 2:
+          counterP2++;
+          break;
+        default:
+        // do nothing
+      }
+      if (counterP1 > 0 && counterP2 > 0) {
+        // no player can complete this line => no value added
+      } else if (counterP1 > 0) {
+        switch (counterP1) {
+          case 1:
+            lineVal.player1 += this.evals.row1;
+            break;
+          case 2:
+            lineVal.player1 += this.evals.row2;
+            break;
+          case 3:
+            lineVal.player1 += this.evals.row3;
+            break;
+          case 4:
+            lineVal.player1 = this.evals.row4;
+            // console.log("win1");
+            break;
+          default:
+          //do nothing
+        }
+      } else if (counterP2 > 0) {
+        switch (counterP2) {
+          case 1:
+            lineVal.player2 += this.evals.row1;
+            break;
+          case 2:
+            lineVal.player2 += this.evals.row2;
+            break;
+          case 3:
+            lineVal.player2 += this.evals.row3;
+            break;
+          case 4:
+            lineVal.player2 = this.evals.row4;
+            // console.log("win2");
+            break;
+          default:
+          //do nothing
+        }
+      }
+    }
 
     this.values[idx] = lineVal;
   }
 
   getCellValues(cell) {
     let cellVal = new Values();
+    let vec = createVector(cell.x, cell.y, cell.z);
     // find all lines containing cell
-
-    // sum values for those lines
-
-    // return values object
+    for (let i = 0; i < this.lines.length; i++) {
+      let update = false;
+      for (let point of this.lines[i]) {
+        if (vec.equals(point)) {
+          // sum values for those lines
+          cellVal.add(this.values[i]);
+        }
+      }
+    }
+    return cellVal;
   }
 
   getBoardValues() {
@@ -77,16 +143,38 @@ class Evaluation {
 
     return boardVal;
   }
+  getBoardValuesString() {
+    let boardVal = this.getBoardValues();
+    return `Player1: ${boardVal.player1}, Player2: ${boardVal.player2}`;
+  }
+
+  getBoardValue(player) {
+    let temp = this.getBoardValues();
+    switch (player) {
+      case 1:
+        return temp.player1 - temp.player2;
+        break;
+      case 2:
+        return temp.player2 - temp.player1;
+        break;
+      default:
+        return undefined;
+    }
+  }
 }
 
 class Values {
-  constructor() {
+  constructor( idx = 0) {
     this.player1 = 0;
     this.player2 = 0;
+    this.idx = idx;
   }
 
   add(other) {
     this.player1 += other.player1;
     this.player2 += other.player2;
+    if(other.player1 > 1000 || other.player2 > 1000){
+      this.idx = other.idx;
+    }
   }
 }
